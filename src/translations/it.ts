2df105c92c6731d22cba6ff4055a83d54e6d2345
M1k3H <49992902+mikikipi@users.noreply.github.com>
2025-08-16 18:57:47 +0200

    it.ts
    Add translations and more customizations

---
+++ src/translations/it.ts
@@ -0,0 +1,74 @@
+export declare const it: {
+    readonly card: {
+        readonly title: "Il Cielo Stanotte";
+        readonly latitude: "Latitudine";
+        readonly longitude: "Longitudine";
+        readonly elevation: "Elevazione";
+        readonly moonPhase: "Fase Lunare";
+        readonly visible: "Visibile";
+        readonly duration: "Durata";
+        readonly altitude: "Altitudine";
+        readonly nakedEye: "Occhio";
+        readonly binocular: "Binocolo";
+        readonly telescope: "Telescopio";
+    };
+    readonly objects: {
+        readonly Sun: "Sole";
+        readonly Moon: "Luna";
+        readonly Mercury: "Mercurio";
+        readonly Venus: "Venere";
+        readonly Mars: "Marte";
+        readonly Jupiter: "Giove";
+        readonly Saturn: "Saturno";
+        readonly Uranus: "Urano";
+        readonly Neptune: "Nettuno";
+        readonly Pluto: "Plutone";
+    };
+    readonly moonPhases: {
+        readonly "New Moon": "Luna Nuova";
+        readonly "Waxing Crescent": "Luna Crescente";
+        readonly "First Quarter": "Primo Quarto";
+        readonly "Waxing Gibbous": "Gibbosa Crescente";
+        readonly "Full Moon": "Luna Piena";
+        readonly "Waning Gibbous": "Gibbosa Calante";
+        readonly "Last Quarter": "Ultimo Quarto";
+        readonly "Waning Crescent": "Luna Calante";
+    };
+    readonly types: {
+        readonly Star: "Stella";
+        readonly Planet: "Pianeta";
+        readonly Moon: "Luna";
+    };
+    readonly time: {
+        readonly h: "ore";
+        readonly m: "minuti";
+    };
+    readonly editor: {
+        readonly latitude: "Latitudine";
+        readonly longitude: "Longitudine";
+        readonly elevation: "Elevazione";
+        readonly elevationUnit: "metri";
+        readonly languageOverride: "Sovrascrivi Linguaggio";
+        readonly useHASetting: "Usa impostazioni HA";
+        readonly showSun: "Mostra Sole";
+        readonly showBelowHorizon: "Mostra oggetti sotto l'orizzonte";
+        readonly showConfiguration: "Mostra Configurazione";
+        readonly colorCustomization: "Configurazione Colore";
+        readonly cardBackground: "Sfondo";
+        readonly cardTitle: "Titolo";
+        readonly objectBackground: "Sfondo oggetto";
+        readonly objectName: "Nome";
+        readonly objectData: "Dati";
+        readonly objectType: "Tipo";
+        readonly reset: "Reimposta";
+        readonly optional: "opzionale";
+        readonly yes: "Si";
+        readonly no: "No";
+    };
+    readonly languages: {
+        readonly en: "Inglese";
+        readonly hi: "Indiano";
+        readonly fr: "Francese";
+        readonly it: "Italiano";
+    };
+};
